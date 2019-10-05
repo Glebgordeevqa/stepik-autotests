2e82ab8d3d230ad5130a9b66eb301a03565addb5
glebgordeevqa <gleb.gordeev@playrix.com>
2019-10-05 17:37:33 +0300

    Upgrade test after conftest!

--- New_tests/lesson36step3.py
+++ New_tests/lesson36step3.py
@@ -1,16 +1,6 @@
 import pytest
-from selenium import webdriver
 import time
 import math
-
-
-@pytest.fixture(scope="function")
-def browser():
-    print("\nstart browser for test..")
-    browser = webdriver.Chrome()
-    yield browser
-    print("\nquit browser..")
-    browser.quit()
 
 @pytest.mark.parametrize('links', ["236895", "236896", "236897", "236898",
                                    "236899", "236903", "236904", "236905"])
