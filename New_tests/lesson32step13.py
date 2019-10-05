b9fabe9978ea7367563b4e27bac6e5b22bde23a8
glebgordeevqa <gleb.gordeev@playrix.com>
2019-10-05 17:31:56 +0300

    Replace new tests

---
+++ New_tests/lesson32step13.py
@@ -0,0 +1,43 @@
+from selenium import webdriver
+import unittest
+
+class TestAbs(unittest.TestCase):
+    def test_first(self):
+        link = "http://suninjuly.github.io/registration1.html"
+        browser = webdriver.Chrome()
+        browser.get(link)
+        browser.implicitly_wait(3)
+        input1 = browser.find_element_by_css_selector(".first_block>.first_class>input.first")
+        input1.send_keys("Ivan")
+        input2 = browser.find_element_by_css_selector(".first_block>.second_class>input.second")
+        input2.send_keys("Petrov")
+        input3 = browser.find_element_by_css_selector(".first_block>.third_class>input.third")
+        input3.send_keys("Smolensk")
+        button = browser.find_element_by_css_selector("button.btn")
+        button.click()
+        welcome_text_elt = browser.find_element_by_tag_name("h1")
+        welcome_text = welcome_text_elt.text
+        self.assertEqual("Congratulations! You have successfully registered!", welcome_text, "noice")
+        browser.quit()
+
+    def test_second(self):
+        link = "http://suninjuly.github.io/registration2.html"
+        browser = webdriver.Chrome()
+        browser.get(link)
+        browser.implicitly_wait(3)
+        input1 = browser.find_element_by_css_selector(".first_block>.first_class>input.first")
+        input1.send_keys("Ivan")
+        input2 = browser.find_element_by_css_selector(".first_block>.second_class>input.second")
+        input2.send_keys("Petrov")
+        input3 = browser.find_element_by_css_selector(".first_block>.third_class>input.third")
+        input3.send_keys("Smolensk")
+        button = browser.find_element_by_css_selector("button.btn")
+        button.click()
+        welcome_text_elt = browser.find_element_by_tag_name("h1")
+        welcome_text = welcome_text_elt.text
+        self.assertEqual("Congratulations! You have successfully registered!", welcome_text, "noice")
+        browser.quit()
+
+if __name__ == '__main__':
+    unittest.main()
+print("")
---
+++ New_tests/lesson36step3.py
@@ -0,0 +1,25 @@
+import pytest
+from selenium import webdriver
+import time
+import math
+
+
+@pytest.fixture(scope="function")
+def browser():
+    print("\nstart browser for test..")
+    browser = webdriver.Chrome()
+    yield browser
+    print("\nquit browser..")
+    browser.quit()
+
+@pytest.mark.parametrize('links', ["236895", "236896", "236897", "236898",
+                                   "236899", "236903", "236904", "236905"])
+def test_green_people(browser, links):
+    link = f"{links}/"
+    answer = math.log(int(time.time()))
+    browser.get(f"https://stepik.org/lesson/{links}/step/1")
+    browser.implicitly_wait(60)
+    browser.find_element_by_css_selector("textarea.textarea.ember-text-area.ember-view").send_keys(f"{answer}")
+    browser.find_element_by_css_selector("button.submit-submission").click()
+    check = browser.find_element_by_css_selector("pre.smart-hints__hint").text
+    assert check == "Correct!", f"Ответ отличается: {check}"
